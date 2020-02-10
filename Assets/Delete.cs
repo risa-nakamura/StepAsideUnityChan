db4afc9800ed5b83c56d34930de366f817898492
risa-nakamura <negi39390822nasi@gmail.com>
2020-02-10 16:00:20 +0900

    StepAsideUnityChan
    課題オブジェクトの破棄まで。

---
+++ Assets/Delete.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Delete : MonoBehaviour {
+
+    private GameObject unitychan;
+    //private float difference;
+    
+
+	// Use this for initialization
+	void Start () {
+        this.unitychan = GameObject.Find("unitychan");
+        //this.difference = unitychan.transform.position.z - gameObject.transform.position.z;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update() {
+        if (unitychan.transform.position.z-10 > gameObject.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
